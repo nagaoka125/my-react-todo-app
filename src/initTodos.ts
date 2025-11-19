b8ad200084fd6b4de76ae6b302938eb2baa1defc
nagaokateppei <171229061+nagaoka125@users.noreply.github.com>
2025-11-20 00:44:30 +0900

    update initTodo

--- src/initTodos.ts
+++ src/initTodos.ts
@@ -4,21 +4,21 @@ import { v4 as uuid } from "uuid"; // v4 を uuid という名前でインポー
 export const initTodos: Todo[] = [
   {
     id: uuid(), // UUID v4 を生成してIDにセット
-    name: "解析2の宿題",
+    name: "test todo 1",
     isDone: false,
     priority: 2,
     deadline: new Date(2024, 10, 2, 17, 30),
   },
   {
     id: uuid(),
-    name: "TypeScriptの勉強 (復習)",
+    name: "test todo 2",
     isDone: true,
     priority: 3,
     deadline: null, // このTodoには期限を設定しない
   },
   {
     id: uuid(),
-    name: "基礎物理学3の宿題",
+    name: "test todo 3",
     isDone: false,
     priority: 1,
     deadline: new Date(2024, 10, 11),
